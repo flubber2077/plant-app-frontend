4b3cb177ff5e74931073f0b65b9fb9a1a048b78c
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-06-01 15:35:10 -0400

    Initial commits

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <h1>Plant App</h1>
+            <p>By Dylan Jordan</p>
+        </header>
+    )
+}
+
+export default Header;
