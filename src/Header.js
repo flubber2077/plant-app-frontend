6049b1829c5400a2783bde5aa44737d6ee533984
Kate Montgomery <katemontgomery431@gmail.com>
2022-06-01 18:36:42 -0400

    well heres that data I worked on gathering for ya

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import React from "react"
 function Header() {
     return (
         <header>
-            <h1>Plant App</h1>
-            <p>By Dylan Jordan</p>
+            <h1><image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1qRAynl-JB2WQvjaT0HgMPNLZQG9GXrXg1A&usqp=CAU" />Plant App</h1>
+            <p>By Dylan Jordan and Kate</p>
         </header>
     )
 }
